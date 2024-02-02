db06832503567dab59919428f84011374a0794e4
alexzhang20 <alex.y.zhang@vanderbilt.edu>
2024-02-01 18:11:39 -0600

    demo 1 progress

---
+++ components/Navbar/routes.ts
@@ -0,0 +1,14 @@
+import {Route} from "@/types/Route";
+
+const routes : Route[] = [
+    {
+        href: '/',
+        text: 'Leaderboard'
+    },
+    {
+        href: '/account',
+        text: 'Professors'
+    },
+]
+
+export default routes
---
+++ theme/color.ts
@@ -0,0 +1,15 @@
+export const colors = {
+    highlight: {
+        50: "#F4F4F4",
+    },
+    background: {
+        500: "#FFFFFF",
+    },
+    navbar: {
+        500: "#F0F0F0",
+    },
+    text: {
+        400: "#b3b3b3",
+        500: "#ffffff",
+    }
+}
