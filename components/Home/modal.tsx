eb6853b464371a22565d36a4d4f05d27f225b158
michellewangg <michellewang215@gmail.com>
2024-02-23 00:37:33 -0600

    Modal updates

--- components/Home/modal.tsx
+++ components/Home/modal.tsx
@@ -8,24 +8,27 @@ import {
     ModalFooter,
     ModalBody,
     ModalCloseButton,
-    Button
+    Text, 
+    Image
 } from '@chakra-ui/react';
 
 function HomeModal() {
     const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true });
-
-    useEffect(() => {
-        onClose(); // Close the modal when component mounts
-    }, []);
     
     return (
-            <Modal isOpen={isOpen} onClose={onClose}>
+            <Modal isOpen={isOpen} onClose={onClose} size = 'lg' isCentered>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Welcome to Vanderbilt Professor Leaderboard!</ModalHeader>
+                    <ModalHeader textAlign="center" fontSize="2xl">Welcome to Vanderbilt Professor Leaderboard!</ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody>
-                        Insert instructions here!
+                    <ModalBody textAlign="center" paddingY={4} marginTop={-4}>
+                        <Image src="/HomePage.png" alt="Sample image of home page"></Image>
+                        <Text color={'#4A5568'} fontSize="md" marginTop={4}>
+                            This is a dynamic leaderboard that is designed to promote professor evaluation 
+                            in a fun and engaging way. You have one vote a day and can use it to either upvote 
+                            your favorite professor or downvote your not-so-favorite professor. Log in with 
+                            your Vanderbilt account to begin voting!
+                        </Text>
                     </ModalBody>
                 </ModalContent>
             </Modal>
