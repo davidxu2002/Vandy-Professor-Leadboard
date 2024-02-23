53d84c1caa0deb2f2dc565d6d7f2c911c20d5c07
michellewangg <michellewang215@gmail.com>
2024-02-23 00:48:08 -0600

    Minor header changes

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -51,7 +51,7 @@ const HomePage = () => {
                 align="center"
                 p={6}
             >
-            <Heading colorScheme='whiteAlpha'>Vanderbilt Professor Leaderboard</Heading>
+            <Heading color='#2B7A78'>Vanderbilt Professor Leaderboard</Heading>
             <HomeModal />
             {professors.slice(0, numProf).map((professor: Professor, index) => (
                 <ProfessorCard
